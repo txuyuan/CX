9aa8b61b8f34a411d74de0c4da8b26bb2c4ae284
Xuyuan <t.xuyuan@gmail.com>
2021-10-08 21:55:28 +0800

    Merge

---
+++ src/main/java/plugin/data/PluginFile.java
@@ -0,0 +1,29 @@
+package plugin.data;
+
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
+import plugin.CX.Main;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.logging.Level;
+
+public class PluginFile {
+
+    public static FileConfiguration getFile(String name) {
+        File file = new File("./plugins/CX", name);
+        FileConfiguration fConfig = (FileConfiguration) YamlConfiguration.loadConfiguration(file);
+        return fConfig;
+    }
+
+    public static void save(FileConfiguration fConfig, String name) {
+        try {
+            File file = new File("./plugins/CX", name);
+            fConfig.save(file);
+        } catch (IOException exception) {
+            Main.getInstance().getLogger().log(Level.SEVERE, "§c(Error)§f §cError writing to disk");
+            exception.printStackTrace();
+        }
+    }
+
+}
